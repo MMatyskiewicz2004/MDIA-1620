cf57c4b85f997ebdd069f63176a87a1b443f304d
MMatyskiewicz2004 <mmatyskiewicz1@my.bcit.caconsole.log(horseName)>
2024-09-20 15:42:54 -0700

    intialized stable

---
+++ stable.js
@@ -0,0 +1,12 @@
+let horseName = "Estabon";
+console.log(horseName);
+horseName = "Justin";
+console.log(horseName)
+
+let horseAge = 5;
+console.log(horseAge);
+
+let isHorseInside = true;
+console.log(isHorseInside);
+
+
